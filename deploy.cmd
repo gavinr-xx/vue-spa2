02d888791a3fe7114698662a73f6eabad25fb5f5
Gavin Rice <rice_gavin@hotmail.com>
2018-08-08 13:36:10 +0100

    a

--- deploy.cmd
+++ deploy.cmd
@@ -1,12 +1,12 @@
 REM call npm install
 REM call npm run build
 
-ECHO %DEPLOYMENT_TARGET%
+REM ECHO %DEPLOYMENT_TARGET%
 
 REM call cd %DEPLOYMENT_TARGET%
 REM for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
 
-REM xcopy /d %DEPLOYMENT_SOURCE%\dist\* %DEPLOYMENT_TARGET%\dist /s /i
+xcopy /d %DEPLOYMENT_SOURCE%\dist\* %DEPLOYMENT_TARGET% /s /i
 REM xcopy /d %DEPLOYMENT_SOURCE%\public\index.html %DEPLOYMENT_TARGET%\index.html*
 REM REM xcopy /d %DEPLOYMENT_SOURCE%\server.js %DEPLOYMENT_TARGET%\server.js*
 REM REM xcopy /d %DEPLOYMENT_SOURCE%\package.json %DEPLOYMENT_TARGET%\package.json*
